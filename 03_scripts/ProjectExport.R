b4bb3c03387f0786d11dabee581b06b8e2738b37
karanmenon-iep <kmenon@economicsandpeace.org>
2024-06-07 12:09:20 +1000

    update

--- 03_scripts/ProjectExport.R
+++ 03_scripts/ProjectExport.R
@@ -1,6 +1,5 @@
 ##### ----- EXPORT CHARTS, MAPS AND TABLES
-#' Note that for the export to work, the standardoutputs and analysis scripts have to 
-#' be run first, in order to create the chart information, plot, and df objects
+
 
 ### --- Libraries and Variables
 
@@ -13,9 +12,6 @@ f_LibraryLoader(tidyverse,
 
 
 
-### --- Section 3 --------------------------------------------------------------
-#' Section 3 Econ Costing Charts
-
 # Export Data
 plot_list <- c("pCHART_PPI")
 
@@ -23,5 +19,6 @@ selected_columns <- c("geoname", "GPI", "GTI", "PPI")
 
 # Write selected columns to an Excel file
 write_selected_columns_to_excel(Indicators.df, selected_columns, "04_outputs/country_report.xlsx")
+#Save Plots
 save_plots_as_png(plot_list)
 
