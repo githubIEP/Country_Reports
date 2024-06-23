c73c3304b9fb44b9619d3f4d7a678d4954ac5300
karanmenon-iep <kmenon@economicsandpeace.org>
2024-06-24 09:34:50 +1000

    Update

--- 03_scripts/ProjectVariables.R
+++ 03_scripts/ProjectVariables.R
@@ -1,29 +1,19 @@
 #### Project Variables #####
 
+
+
+
+
+
+
+
 ###################################################################
 ##  Creating general Variables: COUNTRY NAME & most recent year  ##
 ###################################################################
 
 GEOCODE = "BFA"
 COUNTRY_NAME = "Burkina Faso"
-LATEST_YEAR = 2023
+LATEST_YEAR = 2024
 REGION <- get_region(GEOCODE)
 
-
-
-### Read Files and data ###
-
-#### Charts ######
-CHART_UNIT = "cm"
-
-# Chart Sizes
-CHARTS <- list(
-  small = c(width = 8.45, height = 10),
-  medium = c(width = 12, height = 10),
-  large = c(width = 17.6, height = 10)
-)
-
-# Chart Fonts
-HEAVY_FONT = "Helvetica LT Pro" 
-LIGHT_FONT = "Helvetica LT Pro Light" 
 
